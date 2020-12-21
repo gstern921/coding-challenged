b66f9877792b10efb38e5f18a2f2786f769c4fb7
Gary Stern <gcrunkster@gmail.com>
2020-12-20 19:48:05 -0500

    Add day 5 part 1 and 2

---
+++ advent-of-code/2020/day5/part1/solution.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const path = require("path");
+
+let data = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+
+if (!data) {
+  throw new Error("No Data Found!");
+}
+
+const boardingPasses = data.split("\n");
+
+// console.log(boardingPasses);
+
+let maxSeatId = 0;
+let boardingPass = boardingPasses[0];
+
+for (let boardingPass of boardingPasses) {
+  let rowStart = 0;
+  let rowEnd = 127;
+
+  let columnStart = 0;
+  let columnEnd = 7;
+
+  for (let char of boardingPass) {
+    if (char === "B") {
+      rowStart = calcLowerHalf(rowStart, rowEnd);
+    } else if (char === "F") {
+      rowEnd = calcUpperHalf(rowStart, rowEnd);
+    } else if (char === "R") {
+      columnStart = calcLowerHalf(columnStart, columnEnd);
+    } else if (char === "L") {
+      columnEnd = calcUpperHalf(columnStart, columnEnd);
+    }
+
+    
+  }
+
+  const seatId = calcSeatId(rowStart, columnStart);
+  maxSeatId = Math.max(maxSeatId, seatId);
+
+}
+
+
+function calcLowerHalf(start, end) {
+  return start + Math.ceil((end - start) / 2);
+}
+
+function calcUpperHalf(start, end) {
+  return start + Math.floor((end - start) / 2);
+}
+
+function calcSeatId(row, column) {
+  return row * 8 + column;
+}
---
+++ advent-of-code/2020/day5/part2/solution.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const path = require("path");
+
+let data = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+
+if (!data) {
+  throw new Error("No Data Found!");
+}
+
+const boardingPasses = data.split("\n");
+
+const mySeatId = findMySeatId(boardingPasses);
+
+function calcLowerHalf(start, end) {
+  return start + Math.ceil((end - start) / 2);
+}
+
+function calcUpperHalf(start, end) {
+  return start + Math.floor((end - start) / 2);
+}
+
+function calcSeatId(boardingPass) {
+  let rowStart = 0;
+  let rowEnd = 127;
+
+  let columnStart = 0;
+  let columnEnd = 7;
+
+  for (let char of boardingPass) {
+    if (char === "B") {
+      rowStart = calcLowerHalf(rowStart, rowEnd);
+    } else if (char === "F") {
+      rowEnd = calcUpperHalf(rowStart, rowEnd);
+    } else if (char === "R") {
+      columnStart = calcLowerHalf(columnStart, columnEnd);
+    } else if (char === "L") {
+      columnEnd = calcUpperHalf(columnStart, columnEnd);
+    }
+  }
+
+  return rowStart * 8 + columnStart;
+}
+
+function findMySeatId(boardingPasses) {
+  const seatIds = boardingPasses.map(calcSeatId);
+  seatIds.sort((a, b) => a - b);
+
+  for (let i = 0; i < seatIds.length - 1; i++) {
+    if (seatIds[i + 1] !== seatIds[i] + 1) {
+      return seatIds[i] + 1;
+    }
+  }
+}
