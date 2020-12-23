2a54b8c335dbb6c55fe3067fa304c3ef2f1a3e07
Gary Stern <gcrunkster@gmail.com>
2020-12-23 09:44:52 -0500

    Add day 15 part 1 and 2

---
+++ advent-of-code/2020/day15/part2/solution.js
@@ -0,0 +1,38 @@
+const numbers = [14,3,1,0,9,5];
+
+const spokenNumbers = {};
+
+let lastNumberSpoken;
+
+for (let i = 0; i < 30000000; i++) {
+  if (i % 1000000 === 0) {
+    console.log(i);
+  }
+  if (i < numbers.length) {
+    speakNumber(numbers[i], i, true);
+    lastNumberSpoken = numbers[i];
+  } else {
+    const num = speakNumber(lastNumberSpoken, i - 1);
+    lastNumberSpoken = num;
+  }
+}
+
+console.log(lastNumberSpoken)
+
+function speakNumber(num, index, isStartingNumber = false, spokenNums = spokenNumbers) {
+  if (spokenNums[num] === undefined) {
+    // Number has not been spoken before
+    // Set spokenNums[num] to last called index
+    spokenNums[num] = index;
+  }
+
+  if (spokenNums[num] === index) {
+    const result = isStartingNumber ? num : 0;
+    return result;
+  }
+
+  // Number has been spoken before
+  const diff = index - spokenNums[num];
+  spokenNums[num] = index;
+  return diff;
+}
