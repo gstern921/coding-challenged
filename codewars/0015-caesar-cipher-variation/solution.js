7b526487f770130fece3a069fdc201dd9fd46f3a
Gary Stern <gcrunkster@gmail.com>
2021-01-05 23:02:31 -0500

    Add codewars 0015 solution

--- codewars/0015-caesar-cipher-variation/solution.js
+++ codewars/0015-caesar-cipher-variation/solution.js
@@ -1,11 +1,19 @@
-function transpose(matrix) {
-  const result = [];
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      result[j] = result[j] || [];
-      result[j][i] = matrix[i][j];
+function caesarEncode(phrase, shift) {
+  return phrase
+    .split(' ')
+    .map((word, i) => shiftWord(word, shift + i))
+    .join(' ');
+  
+  function shiftWord(word, shiftDistance) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let result = '';
+    for (let char of word) {
+      const indexOfChar = alphabet.indexOf(char);
+      if (indexOfChar === -1) {
+        throw new Error('Unable to encrypt character ' + char);
+      }
+      result += alphabet[(indexOfChar + shiftDistance) % alphabet.length];
     }
-    
+    return result;
   }
-  return result;
 }
