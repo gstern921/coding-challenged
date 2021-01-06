3b96115b248d208aaf36a828c3c2be646d89be75
Gary Stern <gcrunkster@gmail.com>
2021-01-06 00:00:16 -0500

    add codewars 0016 solution

---
+++ codewars/0016-numbers-plus-pattern/solution.js
@@ -0,0 +1,23 @@
+function pattern(n){
+  
+  let result = new Array(2 * n - 1);
+  
+  for (let i = 1; i <= n; i++) {
+    if (i === n) {
+      result[i - 1] = getOnesDigit(n).toString();
+      for (let j = n - 1; j >= 1; j--) {
+          result[i - 1] = getOnesDigit(j) + result[i - 1] + getOnesDigit(j);
+      }
+    } else {
+      result[i - 1] = ' '.repeat(n - 1) + getOnesDigit(i);
+      result[result.length - i] = ' '.repeat(n - 1) + getOnesDigit(i);
+    }
+  }
+  
+  return result.map(num => num + '\n').join('');
+  
+  function getOnesDigit(num) {
+    return num % 10;
+  }
+
+}
